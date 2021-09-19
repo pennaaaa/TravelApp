c7d8af656c185eb837e5010a4ca98186f127962f
pen <quangkhung125@gmail.com>
2021-09-19 14:50:34 +0700

    Detail city

---
+++ assets/data/activityDetails.js
@@ -0,0 +1,19 @@
+const activityDetails = [
+  {
+    id: "activities-1",
+    title: "Tổng quan",
+  },
+  {
+    id: "activities-2",
+    title: "Khách sạn",
+  },
+  {
+    id: "activities-3",
+    title: "Giải trí",
+  },
+  {
+    id: "activities-4",
+    title: "Ẩm thực",
+  },
+];
+export default activityDetails;
