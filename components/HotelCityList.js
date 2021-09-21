1988313204dd1dc3708c44b1b490ca52944334a9
pen <quangkhung125@gmail.com>
2021-09-21 10:03:02 +0700

    DettailCity

--- components/HotelCityList.js
+++ components/HotelCityList.js
@@ -1,11 +1,51 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import moment from "moment";
+import DateRangePicker from "react-native-daterange-picker";
 
-const HotelCityList = () => {
-  return (
-    <View>
-      <Text>HotelCityList</Text>
-    </View>
-  );
-};
-export default HotelCityList;
+export default class HotelCityList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      displayedDate: moment(),
+    };
+  }
+
+  setDates = (dates) => {
+    this.setState({
+      ...dates,
+    });
+  };
+
+  render() {
+    const { startDate, endDate, displayedDate } = this.state;
+    return (
+      <View style={styles.container}>
+        <DateRangePicker
+          onChange={this.setDates}
+          endDate={endDate}
+          startDate={startDate}
+          displayedDate={displayedDate}
+          range
+        >
+          <Text>Click me!</Text>
+          {/* <Text>start datet : {startDate}</Text> */}
+          {/* <Text>{endDate}</Text> */}
+        </DateRangePicker>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    paddingTop: 20,
+  },
+});
