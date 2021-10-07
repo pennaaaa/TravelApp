fd525fb45448834dbc13b5d6e60041dbec8d9fdb
pen <quangkhung125@gmail.com>
2021-10-07 21:46:50 +0700

    searchView

---
+++ assets/data/kindSearch.js
@@ -0,0 +1,19 @@
+const activityDetails = [
+  {
+    id: "activities-1",
+    title: "Điểm đến",
+  },
+  {
+    id: "activities-2",
+    title: "Khách sạn",
+  },
+  {
+    id: "activities-3",
+    title: "Nhà hàng",
+  },
+  {
+    id: "activities-4",
+    title: "Giải trí",
+  },
+];
+export default activityDetails;
