ace61df8518633191fde83930dc6e0a74a6594b3
vnd <vndat74@gmail.com>
2021-12-24 10:57:56 +0700

    update

--- components/SearchView.js
+++ components/SearchView.js
@@ -4,11 +4,11 @@ import {
   Text,
   Dimensions,
   StyleSheet,
-  SafeAreaView,
   TouchableOpacity,
   Image,
 } from "react-native";
 import SearchBar from "react-native-dynamic-search-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { FlatList } from "react-native-gesture-handler";
 import colors from "../assets/color/colors";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
